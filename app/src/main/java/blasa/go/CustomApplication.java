f8837772621b10c2f7472c0f8c1460cca2087129
pooja29 <112708703+poojaverma2911@users.noreply.github.com>
2024-07-29 17:28:48 +0530

    Update CustomApplication.java

--- app/src/main/java/blasa/go/CustomApplication.java
+++ app/src/main/java/blasa/go/CustomApplication.java
@@ -4,9 +4,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.firebase.client.Firebase;
 
-/**
- * Created by omarelamri on 04/04/2018.
- */
+ 
 
 public class CustomApplication extends android.app.Application {
 
